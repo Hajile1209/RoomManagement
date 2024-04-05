7810837bd27aeced563048f983b6bed894490e76
Elijah <Elijah@ELIJAH>
2024-04-05 16:36:55 +0800

    Add project files.

---
+++ RoomManagementBL/Room.cs
@@ -0,0 +1,14 @@
+﻿namespace RoomManagementBL
+{
+    public class Room
+    {
+        public string roomNo;
+        public string name;
+        public string phonenum;
+        public string numRent;
+        public string pets;
+        public string Ac;
+
+
+    }
+}
